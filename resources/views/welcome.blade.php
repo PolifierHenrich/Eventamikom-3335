69db31a37b47e8b2e36f5de4298b335dfc17ba93
Wijdan Ula Rizki Siregar <rizkigagah16@gmail.com>
2026-04-09 21:45:10 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,12 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        <div style="text-align: center; margin-top: 50px; font-family: sans-serif;">
+        <h1>Tugas 1 Laravel</h1>
+        <h2>Nama: {{ $nama }}</h2>
+        <h3>NIM: {{ $nim }}</h3>
+    <hr>
+</div>
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">

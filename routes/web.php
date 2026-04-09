9c8ca0d0a15c63ce25281a83b570c28fb4b4b506
Wijdan Ula Rizki Siregar <rizkigagah16@gmail.com>
2026-04-09 21:12:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mahasiswa', function () {
+    return response()->json([
+        'nama' => 'Wijdan Ula Rizki',
+        'nim'  => '24.12.3335',
+    ]);
+});
